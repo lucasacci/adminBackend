9c4678b8744418a4f658c32d85b5a84f2df3ebe0
Rocha Felipe <rochaxx29@gmail.com>
2024-11-21 20:29:36 -0300

    Agregar Clases para el Mail

---
+++ src/main/java/com/apirest/apiadmin/models/EmailModel.java
@@ -0,0 +1,31 @@
+package com.apirest.apiadmin.models;
+
+public class EmailModel {
+    private String mail_to;
+    private String subject;
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMail_to() {
+        return mail_to;
+    }
+
+    public void setMail_to(String mail_to) {
+        this.mail_to = mail_to;
+    }
+}
