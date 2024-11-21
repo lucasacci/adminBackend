ebdb4dab3dda55c485d662a78379484e57aaf9ec
Rocha Felipe <rochaxx29@gmail.com>
2024-11-21 20:48:37 -0300

    Agregado de dependencias

--- src/main/java/com/apirest/apiadmin/services/EmailServiceImpl.java
+++ src/main/java/com/apirest/apiadmin/services/EmailServiceImpl.java
@@ -3,9 +3,13 @@ package com.apirest.apiadmin.services;
 
 import com.apirest.apiadmin.models.EmailModel;
 import com.apirest.apiadmin.repositories.IEmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 
 public class EmailServiceImpl implements IEmailService {
     private final JavaMailSender mailSender;
@@ -17,7 +21,7 @@ public class EmailServiceImpl implements IEmailService {
     }
 
     @Override
-    public void sendEmail(EmailModel email) throws MessagingException{
+    public void sendEmail(EmailModel email) throws MessagingException {
         try {
             MimeMessage mimeMailMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMailMessage,
